ff18f81197ad5108f65cb054e7153e0c1e0a2d68
Benedikt Linse <benedikt@confluent.io>
2025-03-31 11:46:15 +0200

    adding script for checking cxid and zxid on all source and destination zks

---
+++ observer-migration/exec-format-tx-log.sh
@@ -0,0 +1,13 @@
+kubectl cp format-zk-transaction-log.sh zk-source-0:/tmp/
+kubectl cp format-zk-transaction-log.sh zk-source-1:/tmp/
+kubectl cp format-zk-transaction-log.sh zk-source-2:/tmp/
+kubectl cp format-zk-transaction-log.sh zk-destination-0:/tmp/
+kubectl cp format-zk-transaction-log.sh zk-destination-1:/tmp/
+kubectl cp format-zk-transaction-log.sh zk-destination-2:/tmp/
+
+kubectl exec -it zk-source-0 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-source-1 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-source-2 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-destination-0 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-destination-1 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-destination-2 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
