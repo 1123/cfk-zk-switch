263494931cd326591e17faaa3d0542739cf59c68
T08184A <martin.dengler@datev.de>
2025-03-31 14:53:22 +0200

    MDE

--- observer-migration/check-zk-status-local.sh
+++ observer-migration/check-zk-status-local.sh
@@ -1,4 +1,4 @@
-kubectl exec -it zk-dummy-0 -- sh -c 'echo stat | nc zk-destination-0.zk-destination.confluent.svc.cluster.local 2181'
-kubectl exec -it zk-dummy-0 -- sh -c 'echo stat | nc zk-destination-1.zk-destination.confluent.svc.cluster.local 2181'
-kubectl exec -it zk-dummy-0 -- sh -c 'echo stat | nc zk-destination-2.zk-destination.confluent.svc.cluster.local 2181'
+kubectl exec -it zk-dummy-0 -- sh -c 'echo stat | nc zk-destination-0.zk-destination.martin.svc.cluster.local 2181'
+kubectl exec -it zk-dummy-0 -- sh -c 'echo stat | nc zk-destination-1.zk-destination.martin.svc.cluster.local 2181'
+kubectl exec -it zk-dummy-0 -- sh -c 'echo stat | nc zk-destination-2.zk-destination.martin.svc.cluster.local 2181'
 
--- observer-migration/test-zk-destination.sh
+++ observer-migration/test-zk-destination.sh
@@ -1,2 +1,2 @@
 kubectl exec -it zk-dummy-0 -- \
-  echo stat \| nc zk-destination-0.zk-destination.confluent.svc.cluster.local 2181
+  /bin/bash -c 'echo stat | nc zk-destination-0.zk-destination.martin.svc.cluster.local 2181'
