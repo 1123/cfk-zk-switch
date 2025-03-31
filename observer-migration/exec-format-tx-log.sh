4cb4f32765bbcddeb48d6b5aa00913731af755f4
T08184A <martin.dengler@datev.de>
2025-03-31 15:47:18 +0200

    MDE

--- observer-migration/exec-format-tx-log.sh
+++ observer-migration/exec-format-tx-log.sh
@@ -5,9 +5,9 @@ kubectl cp format-zk-transaction-log.sh zk-destination-0:/tmp/
 kubectl cp format-zk-transaction-log.sh zk-destination-1:/tmp/
 kubectl cp format-zk-transaction-log.sh zk-destination-2:/tmp/
 
-kubectl exec -it zk-source-0 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
-kubectl exec -it zk-source-1 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
-kubectl exec -it zk-source-2 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
-kubectl exec -it zk-destination-0 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
-kubectl exec -it zk-destination-1 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
-kubectl exec -it zk-destination-2 -- sh -c '/tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-source-0 -- sh -c 'chmod +x /tmp/format-zk-transaction-log.sh; /tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-source-1 -- sh -c 'chmod +x /tmp/format-zk-transaction-log.sh; /tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-source-2 -- sh -c 'chmod +x /tmp/format-zk-transaction-log.sh; /tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-destination-0 -- sh -c 'chmod +x /tmp/format-zk-transaction-log.sh; /tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-destination-1 -- sh -c 'chmod +x /tmp/format-zk-transaction-log.sh; /tmp/format-zk-transaction-log.sh | tail -2'
+kubectl exec -it zk-destination-2 -- sh -c 'chmod +x /tmp/format-zk-transaction-log.sh; /tmp/format-zk-transaction-log.sh | tail -2'
--- observer-migration/test-zk-destination.sh
+++ observer-migration/test-zk-destination.sh
@@ -1,2 +1,8 @@
 kubectl exec -it zk-dummy-0 -- \
   /bin/bash -c 'echo stat | nc zk-destination-0.zk-destination.martin.svc.cluster.local 2181'
+
+kubectl exec -it zk-dummy-0 -- \
+  /bin/bash -c 'echo stat | nc zk-destination-1.zk-destination.martin.svc.cluster.local 2181'
+
+kubectl exec -it zk-dummy-0 -- \
+  /bin/bash -c 'echo stat | nc zk-destination-2.zk-destination.martin.svc.cluster.local 2181'
